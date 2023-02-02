8d496b933ca1863cfe5f56903e5c3ea998ea526b
Kamran Qadir <qadir0108@hotmail.com>
2023-02-02 13:22:56 +0500

    movie crud

--- MvcMovie/MvcMovie/Program.cs
+++ MvcMovie/MvcMovie/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
 namespace MvcMovie
 {
     public class Program
@@ -5,6 +8,8 @@ namespace MvcMovie
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
